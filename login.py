1ab56f39dc423bb23d83dc99fc5467259ccbe710
forrestlmj <18810276930@163.com>
2019-03-19 11:13:09 +0800

    增加时间延迟的while和sleep循环

--- login.py
+++ login.py
@@ -36,15 +36,35 @@ def qichacha_login(id,password):
             print("加载验证码中")
             pass
         except Exception as e:
+            print(e)
             pass
         sleep(1)
     identifying_code = TestFunc(identifying_code_pic)
     # 填写验证码
-    yzm = b1.find_element_by_xpath("//input[@id='nc_1_captcha_input']")
-    sleep(3)
-    yzm.send_keys(identifying_code.value)
+    while(True):
+        try:
+            yzm = b1.find_element_by_xpath("//input[@id='nc_1_captcha_input']")
+            yzm.send_keys(identifying_code.value)
+            break
+        except NoSuchElementException as nSuch:
+            print("加载验证码输入框中")
+            pass
+        except Exception as e:
+            print(e)
+            pass
+        sleep(1)
     # 点击验证码确认按钮
-    b1.find_element_by_xpath("//div[@id='nc_1_scale_submit']").click()
+    while(True):
+        try:
+            b1.find_element_by_xpath("//div[@id='nc_1_scale_submit']").click()
+            break
+        except NoSuchElementException as nSuch:
+            print("点击验证码确认按钮中")
+            pass
+        except Exception as e:
+            print(e)
+            pass
+        sleep(1)
     while(True):
         sleep(3)
         if 'user_login' not in b1.current_url:
@@ -56,7 +76,7 @@ def qichacha_login(id,password):
     # 点击
     close_weixin()
     search()
-    get_search_cookie()
+    return get_search_cookie()
 def get_search_cookie():
     i = 0
     while(i<5):
@@ -72,35 +92,45 @@ def get_search_cookie():
 def close_weixin():
     try_time = 0
     while(try_time<10):
-        sleep(3)
+        sleep(4)
         print(try_time)
         try:
             # b1.find_element_by_xpath("//div[@id='bindWxQrcode']")
             b1.find_element_by_xpath("//button[@class='close']").click()
             print("成功关闭微信界面")
             return
-        except:
+        except NoSuchElementException as nSuch:
+            print("暂时无微信界面")
+        except Exception as e:
+            print(e)
             pass
 
-            print("无微信界面")
         try_time += 1
+    print("一直无微信界面继续操作...")
 def search(word = "京东"):
 
     b1.find_element_by_xpath("//input[@id='searchkey']").send_keys(word)
     # 注意这里停顿 保证搜索按钮可以加载
-    sleep(3)
-    c = b1.find_element_by_xpath("//span[@class='input-group-btn']")
-    c.click()
+    while(True):
+        sleep(3)
+        try:
+            c = b1.find_element_by_xpath("//span[@class='input-group-btn']")
+            c.click()
+            break
+        except NoSuchElementException as nSuch:
+            print("点击搜索键中")
+            pass
+        except Exception as e:
+            print(e)
+            pass
+
 def while_wait():
     while(True):
         sleep(0.5)
 
 
         break
-
-print("xx")
-if __name__ == "__main__":
-    b1 = webdriver.Firefox(executable_path="D:/geckodriver.exe")
-    b1.get("https://www.qichacha.com/user_login")
-    qichacha_login("18224089826","123456")
+# if __name__ == "__main__":
+b1 = webdriver.Firefox(executable_path="D:/geckodriver.exe")
+b1.get("https://www.qichacha.com/user_login")
 
