bcc917a09da6236cdbd8dd98f9dabaf60a14fa1f
forrestlmj <18810276930@163.com>
2019-04-11 15:56:25 +0800

    加入账号访问频率过多问题解决方案

--- login.py
+++ login.py
@@ -21,21 +21,32 @@ class Qichacha:
         while (True):
             try:
                 slider = self.b1.find_element_by_xpath("//span[@id='nc_1_n1z']")
-                ActionChains(self.b1).click_and_hold(slider).perform()
                 sleep(1)
-                ActionChains(self.b1).move_by_offset(xoffset=slider_len, yoffset=0).perform()
+                action = ActionChains(self.b1)
+                action.click_and_hold(slider).perform()
+                action.drag_and_drop_by_offset(slider, xoffset=slider_len, yoffset=0).perform()
                 sleep(1)
-                break
+                try:
+                    if self.b1.find_element_by_xpath("//div[@id='nc_1__imgCaptcha_img']//img"):
+                        break
+                except NoSuchElementException as e:
+                        print("定位验证码")
             except NoSuchElementException as e:
                 print("定位验证码")
             except Exception as e:
                 print(e)
+
         # 截图验证码，下载验证码，使用接码平台来识别验证码
         identifying_code_pic = "yzm/" + time.strftime('%Y%m%d-%H%M%S', time.localtime(time.time())) + ".png"
         while (True):
             try:
-                self.b1.find_element_by_xpath("//div[@id='nc_1__imgCaptcha_img']//img").screenshot(identifying_code_pic)
-                break
+                self.b1.find_element_by_xpath("//div[@id='nc_1__imgCaptcha_img']//img").screenshot(
+                    identifying_code_pic)
+                try:
+                    if self.b1.find_element_by_xpath("//input[@id='nc_1_captcha_input']"):
+                        break
+                except NoSuchElementException as e:
+                    pass
             except NoSuchElementException as nSuch:
                 print("加载验证码中")
                 pass
@@ -49,7 +60,11 @@ class Qichacha:
             try:
                 yzm = self.b1.find_element_by_xpath("//input[@id='nc_1_captcha_input']")
                 yzm.send_keys(identifying_code.value)
-                break
+                try:
+                    if self.b1.find_element_by_xpath("//div[@id='nc_1_scale_submit']"):
+                        break
+                except NoSuchElementException as e:
+                    pass
             except NoSuchElementException as nSuch:
                 print("加载验证码输入框中")
                 pass
@@ -138,71 +153,20 @@ class Qichacha:
             try:
                 c = self.b1.find_element_by_xpath("//span[@class='input-group-btn']")
                 c.click()
-                if "index_verify?" in self.b1.current_url:
-                    # https://www.qichacha.com/index_verify?type=companysearch&back=/search?key=%E4%BA%AC%E4%B8%9C
-                    # 登陆过于频繁情况。
-                    while (True):
-                        try:
-                            slider = self.b1.find_element_by_xpath("//div[@class='box m-t-xl m-b-xl']//span[@id='nc_1_n1z']")
-                            sleep(1)
-                            action = ActionChains(self.b1)
-                            action.click_and_hold(slider).perform()
-                            action.drag_and_drop_by_offset(slider,xoffset=263, yoffset=0).perform()
-                            if self.b1.find_element_by_xpath("//div[@id='nc_1__imgCaptcha_img']//img"):
-                                break
-                        except NoSuchElementException as e:
-                            print("定位验证码")
-                        except Exception as e:
-                            print(e)
-                    # 截图验证码，下载验证码，使用接码平台来识别验证码
-                    identifying_code_pic = "yzm/" + time.strftime('%Y%m%d-%H%M%S', time.localtime(time.time())) + ".png"
-                    while (True):
-                        try:
-                            self.b1.find_element_by_xpath("//div[@id='nc_1__imgCaptcha_img']//img").screenshot(
-                                identifying_code_pic)
-                            if self.b1.find_element_by_xpath("//input[@id='nc_1_captcha_input']"):
-                                break
-                        except NoSuchElementException as nSuch:
-                            print("加载验证码中")
-                            pass
-                        except Exception as e:
-                            print(e)
-                            pass
-                        sleep(1)
-                    identifying_code = TestFunc(identifying_code_pic)
-                    # 填写验证码
-                    while (True):
-                        try:
-                            yzm = self.b1.find_element_by_xpath("//input[@id='nc_1_captcha_input']")
-                            yzm.send_keys(identifying_code.value)
-                            if self.b1.find_element_by_xpath("//div[@id='nc_1_scale_submit']"):
-                                break
-                        except NoSuchElementException as nSuch:
-                            print("加载验证码输入框中")
-                            pass
-                        except Exception as e:
-                            print(e)
-                            pass
-                        sleep(1)
-                    # 点击验证码确认按钮
-                    while (True):
-                        try:
-                            self.b1.find_element_by_xpath("//div[@id='nc_1_scale_submit']").click()
-                        except NoSuchElementException as nSuch:
-                            print("点击验证码确认按钮中")
-                            pass
-                        except Exception as e:
-                            print(e)
-                            pass
-                        sleep(1)
-                else:
-                    break
+
             except NoSuchElementException as nSuch:
                 print("点击搜索键中")
                 pass
             except Exception as e:
                 print(e)
                 pass
+            # https://www.qichacha.com/index_verify?type=companysearch&back=/search?key=%E4%BA%AC%E4%B8%9C
+            # 登陆过于频繁情况。
+            if "index_verify?" in self.b1.current_url:
+
+                self.yanzheng(263)
+            else:
+                break
 
     def while_wait(self):
         while (True):
