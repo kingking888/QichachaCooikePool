bc8cf4f1c4805d559ee74798b127d7c1fb4a61d7
forrestlmj <18810276930@163.com>
2019-04-12 16:48:32 +0800

    上传cookie函数优化增加banned字段。

--- cookie_pool.py
+++ cookie_pool.py
@@ -7,16 +7,18 @@ import json
 from setting import SOLR_LOCAL
 
 
-def update_solr(user_name,user_cookie,isValid="True"):
+def update_solr(user_name,user_cookie,isLogIn="True"):
     params = {"overwrite": "true", "commitWithin": 1000, "wt": "json"}
     solr_into_url = SOLR_LOCAL
     headers = {"Content-Type": "application/json"}
     solr_data = [{"id": user_name,
                   "userCookie": {"set":user_cookie},
+                  "loginTime":{"set":time.strftime('%Y%m%d%H%M%S',time.localtime(time.time()))},
+                  "lastUsedTime": {"set": time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()))},
+                  "isLogIn": {"set":isLogIn},
+                  "isBanned": {"set": "False"},
                   "logoutTime":{"set":0},
-                  "createTime":{"set":time.strftime('%Y%m%d%H%M%S',time.localtime(time.time()))},
-                  "lastUsedTime":{"set":time.strftime('%Y%m%d%H%M%S',time.localtime(time.time()))},
-                  "isValid":{"set":isValid}}
+                  "BannedTime":{"set":0}}
                   ]
     a = json.dumps(solr_data, ensure_ascii=False)
     re = requests.post(solr_into_url, data=a,params=params,headers=headers)
